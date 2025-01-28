3bde9c7c1ba838eabaf9de907311f4187f769fbe
Olivia Emms <123oliviaemms123@gmail.com>
2025-01-28 00:08:19 +0000

    A start made on basic SQL practises.

--- Basics.sql
+++ Basics.sql
@@ -1,8 +1,20 @@
 
 CREATE TABLE employee (
-    ID int,
-    FirstName varchar(255),
-    LastName varchar(255),
-    Address varchar(255),
-    City VARCHAR
-)
+    empID int NOT NULL UNIQUE,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    Address varchar(255) NOT NULL,
+    Department varchar(255) NOT NULL,
+    PRIMARY KEY (empID),
+    FOREIGN KEY (depID) REFERENCES department(depID)
+);
+
+ALTER TABLE employee ADD Age int;
+
+CREATE TABLE department (
+    depID int NOT NULL UNIQUE,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (depID, Name)
+);
+
+SELECT * FROM employee;
